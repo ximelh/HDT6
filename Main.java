8d452ecfef5569724c4f23ad24344cc9ccdfa221
ximelh <ximenaloarca@gmail.com>
2022-03-13 17:40:45 -0600

    Comentarios

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/**
+ * Main.java
+ * Clase principal
+ */
 public class Main {
-    public static void main(String args[]){}
+    public static void main(String args[])
+    {
+
+    }
 }
--- MapFactory.java
+++ MapFactory.java
@@ -2,19 +2,29 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
+/**
+ * MapFactory.java
+ * @author Ximena Loarca
+ * Clase Factory que devuelve el Map con la implementacion deseada
+ */
+
 
 public class MapFactory<K,V>
 {
     private Map<K,V> MyMap;
-
+/**
+ * Devuelve el Map con la implementacion deseada
+ * @param entry
+ * @return
+ */
 public Map<K,V> returnMap(String entry) {
-   
+   //Seleccion de la implementacion
 	if (entry.equalsIgnoreCase("HM"))
-      return this.MyMap = new HashMap<K,V>(); 
+      return this.MyMap = new HashMap<K,V>(); //Devuelve HashMap
     else if (entry.equalsIgnoreCase("LH"))
-        return this.MyMap = new LinkedHashMap<K,V>(); 
+        return this.MyMap = new LinkedHashMap<K,V>(); //Devuelve LinkedHashMap
 	else
-      return this.MyMap = new TreeMap<K,V>(); 
+      return this.MyMap = new TreeMap<K,V>(); //Devuelve TreeMap
    }
 }
 
